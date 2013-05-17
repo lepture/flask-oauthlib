29be7814a6140f1eb8cd1377fbec0370e0a2f3ab
Hsiaoming Yang <me@lepture.com>
2013-05-17 22:59:28 +0800

    sketch for provider

--- flask_oauthlib/provider.py
+++ flask_oauthlib/provider.py
@@ -7,3 +7,27 @@ Implemnts OAuth2 provider support for Flask.
 
 :copyright: (c) 2013 by Hsiaoming Yang.
 """
+
+
+class OAuth(object):
+    """Provide secure services using OAuth2.
+
+    ::
+
+        @app.route('/statuses/timeline')
+        @oauth.protect(scope='statuses')
+        def timeline():
+            ...
+    """
+
+    def __init__(self, scopes=None):
+        self.scopes = scopes
+
+    def protect(self, scope=None, methods=None):
+        pass
+
+    def access_token_handler(self, f):
+        pass
+
+    def refresh_token_handler(self, f):
+        pass
