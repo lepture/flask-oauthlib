b8a236a98e9e5c516bb6981bcc137cd09f3d0151
Hsiaoming Yang <me@lepture.com>
2013-05-28 20:02:22 +0800

    prepare for request validator

--- flask_oauthlib/provider.py
+++ flask_oauthlib/provider.py
@@ -34,8 +34,9 @@ class OAuth(object):
             return client
 
         @oauth.tokengetter
-        def bearer_token(token_id):
-            token = get_token_model(token_id)
+        def bearer_token(access_token=None, refresh_token=None):
+            # implemented get token by access token or refresh token
+            token = get_token_model(access_token, refresh_token)
             # Token is an object, it should has `client_id`
             return token
 
@@ -151,12 +152,49 @@ class OAuth(object):
 
 
 class OAuthRequestValidator(RequestValidator):
+    """Subclass of Request Validator.
+
+    :param clientgetter: a function to get the client object
+    :param tokengetter: a function to get the token object
+
+    The `client` is an object contains at least:
+
+        - client_id
+        - client_secret
+        - redirect_uris
+        - default_redirect_uri
+        - default_scopes
+    """
     def __init__(self, clientgetter, tokengetter):
         self._clientgetter = clientgetter
         self._tokengetter = tokengetter
 
+    def confirm_scopes(self, refresh_token, scopes, request, *args, **kwargs):
+        tok = self._tokengetter(refresh_token=refresh_token)
+        return set(tok.scopes) == set(scopes)
+
+    def get_default_redirect_uri(self, client_id, request, *args, **kwargs):
+        client = self._clientgetter(client_id)
+        return client.default_redirect_uri
+
+    def get_default_scopes(self, client_id, request, *args, **kwargs):
+        client = self._clientgetter(client_id)
+        return client.default_scopes
+
+    def invalidate_authorization_code(self, client_id, code, request,
+                                      *args, **kwargs):
+        # TODO
+        pass
+
+    def save_authorization_code(self, client_id, code, request,
+                                *args, **kwargs):
+        # TODO
+        pass
+
     def validate_bearer_token(self, token, scopes, request):
-        tok = self._tokengetter(token)
+        tok = self._tokengetter(access_token=token)
+        if not tok:
+            return False
         return set(tok.scopes).issuperset(set(scopes))
 
     def validate_client_id(self, client_id, request, *args, **kwargs):
@@ -165,13 +203,40 @@ class OAuthRequestValidator(RequestValidator):
             return True
         return False
 
+    def validate_code(self, client_id, code, client, request, *args, **kwargs):
+        # TODO
+        pass
+
     def validate_grant_type(self, client_id, grant_type, client, request,
                             *args, **kwargs):
         if not hasattr(client, 'grant_types'):
             return True
         return grant_type in client.grant_types
 
-    def validate_code(self, client_id, code, client, request, *args, **kwargs):
+    def validate_redirect_uri(self, client_id, redirect_uri, request,
+                              *args, **kwargs):
+        client = self._clientgetter(client_id)
+        # TODO
+        return redirect_uri in client.redirect_uris
+
+    def validate_refresh_token(self, refresh_token, client, request,
+                               *args, **kwargs):
+        # TODO
+        pass
+
+    def validate_response_type(self, client_id, response_type, client, request,
+                               *args, **kwargs):
+        # TODO
+        pass
+
+    def validate_scopes(self, client_id, scopes, client, request,
+                        *args, **kwargs):
+        # TODO
+        pass
+
+    def validate_user(self, username, password, client, request,
+                      *args, **kwargs):
+        # TODO
         pass
 
 
