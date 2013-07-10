8509fc9e34df65a8c4887f5be4887c7e3e51cac2
Randy Topliffe <randytopliffe@gmail.com>
2013-07-10 16:39:53 -0400

    Binding documentation complete. Some small fixes were also made.

--- docs/additional.rst
+++ docs/additional.rst
@@ -1,7 +1,7 @@
 Additional Features
 ===================
 
-This documentation covers some additional features, they are not required,
+This documentation covers some additional features. They are not required,
 but they may be very helpful.
 
 Request Hooks
@@ -29,3 +29,117 @@ And you can also modify the response with after_request::
         if request.user in black_list:
             return False, request
         return valid, oauth
+
+Bindings
+--------
+
+Bindings are objects you can use to configure flask-oauthlib for use with
+various data stores. They allow you to define the required getters and setters
+for each data store with little effort.
+
+SQLAlchemy OAuth2
+`````````````````
+
+:class:`SQLAlchemyBinding` sets up getters and setters for storing the user,
+client, token and grant with SQLAlchemy, with some sane defaults. To use this
+class you'll need to create a SQLAlchemy model for each object. You can find
+examples of how to setup your SQLAlchemy models here: ref:`oauth2`.
+
+Once you've setup your models you'll need to create a function that returns
+a SQLAlchemy session. Here is an example::
+
+    def get_session():
+        return db.session
+
+This makes sure that the setter and getter functions are using the correct
+session on each call.
+
+You'll also need to provide another function which returns the currently
+logged-in user.
+
+An example of how to use :class:`SQLAlchemyBinding`::
+
+    oauth = OAuth2Provider(app)
+
+    SQLAlchemyBinding(oauth, get_session, user=User, client=Client,
+                      token=Token, grant=Grant, current_user=current_user)
+
+Any of the classes can be omitted if you wish to register the getters and
+setters yourself::
+
+    oauth = OAuth2Provider(app)
+
+    SQLAlchemyBinding(oauth, get_session, user=User, client=Client,
+                      token=Token)
+
+    @oauth.grantgetter
+    def get_grant(client_id, code):
+        pass
+
+    @oauth.grantsetter
+    def set_grant(client_id, code, request, *args, **kwargs):
+        pass
+
+    # register tokensetter with oauth but keeping the tokengetter
+    # registered by `SQLAlchemyBinding`
+    # You would only do this for the token and grant since user and client
+    # only have getters
+    @oauth.tokensetter
+    def set_token(token, request, *args, **kwargs):
+        pass
+
+`current_user` is only used with the Grant bindings, therefore if you are going
+to register your own grant getter and setter you don't need to provide that
+function.
+
+Grant Cache
+```````````
+
+Since the life of a Grant token is very short (usually about 100 seconds),
+storing it in a relational database is inefficient.
+The :class:`GrantCacheBinding` allows you to more efficiently cache the grant
+token using Memcache, Redis, or some other caching system.
+
+An example::
+
+    oauth = OAuth2Provider(app)
+    config = {'OAUTH2_CACHE_TYPE': 'redis'}
+
+    GrantCacheBinding(app, oauth, current_user, config=config)
+
+- `app`: flask application
+- `oauth`: OAuth2Provider instance
+- `current_user`: a function that returns the current user
+- `config`: Any extra configuration
+
+The configuration options are described below. The :class:`GrantCacheBinding`
+will use the configuration options from `Flask-Cache` if they are set, else it
+will set them to the following defaults. Any configuration specific to
+:class:`GrantCacheBinding` will take precedence over any `Flask-Cache`
+configuration that has been set.
+
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **Option**                         | **Description**                                                                                                   | **Flask-Cache Default** | **Default** |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_DEFAULT_TIMEOUT**   | The default timeout that is used. A grant token expires after this length of time.                                | CACHE_DEFAULT_TIMEOUT   | 100s        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_THRESHOLD**         | The maximum number of items the cache stores before it starts deleting some                                       | CACHE_THRESHOLD         | 500s        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_KEY_PREFIX**        | A prefix that is added before all keys                                                                            | CACHE_KEY_PREFIX        | None        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_MEMCACHED_SERVERS** | A list or tuple of server addresses or alternatively a :class:`memcache.Client` or a compatible client            | CACHE_MEMCACHED_SERVERS | None        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_REDIS_HOST**        | Address of the Redis server or an object which API is compatible with the official Python Redis client (redis-py) | CACHE_REDIS_HOST        | localhost   |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_REDIS_PORT**        | Port number on which Redis server listens for connections                                                         | CACHE_REDIS_PORT        | 6379        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_REDIS_PASSWORD**    | Password authentication for the Redis server                                                                      | CACHE_REDIS_PASSWORD    | None        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_REDIS_DB**          | Database (zero-based numeric index) on Redis Server to connect                                                    | CACHE_REDIS_DB          | 0           |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_DIR**               | The directory where cache files are stored                                                                        | CACHE_DIR               | None        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_MODE**              | The file mode wanted for the cache files                                                                          | CACHE_MODE              | 0600        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
+| **OAUTH2_CACHE_TYPE**              | The cache system to be used (null, simple, redis, memcache, filesystem)                                           | CACHE_TYPE              | null        |
++------------------------------------+-------------------------------------------------------------------------------------------------------------------+-------------------------+-------------+
--- flask_oauthlib/contrib/bindings.py
+++ flask_oauthlib/contrib/bindings.py
@@ -3,7 +3,7 @@
     flask_oauthlib.contrib.bindings
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    SQLAlchemy and Cache support for OAuth2 provider.
+    SQLAlchemy and Grant-Caching for OAuth2 provider.
 
     contributed by: Randy Topliffe
 """
@@ -26,14 +26,15 @@ log = logging.getLogger('flask_oauthlib')
 
 
 class Grant(object):
-    """Grant object returned back to the provider
+    """Grant is only used by `GrantCacheBinding` to store the data
+    returned from the cache system.
 
-    :param cache: cache instance
-    :param client_id:
-    :param code:
-    :param redirect_uri:
-    :param scopes: a space delimited list of scopes
-    :param user: user object returned from self.current_user
+    :param cache: Werkzeug cache instance
+    :param client_id: ID of the client
+    :param code: A random string
+    :param redirect_uri: A URI string
+    :param scopes: A space delimited list of scopes
+    :param user: the authorizatopm user
     """
 
     def __init__(self, cache=None, client_id=None, code=None,
@@ -46,7 +47,10 @@ class Grant(object):
         self.user = user
 
     def delete(self):
-        """Removes itself from the cache"""
+        """Removes itself from the cache
+
+        Note: This is required by the oauthlib
+        """
         log.debug("Deleting grant")
         log.debug("Code: {0}".format(self.code))
         log.debug("Client id: {0}".format(self.client_id))
@@ -55,7 +59,7 @@ class Grant(object):
 
     @property
     def key(self):
-        """String used as the key for the cache"""
+        """The string used as the key for the cache"""
         key = '{code}{client_id}'.format(
             code=self.code,
             client_id=self.client_id
@@ -66,31 +70,111 @@ class Grant(object):
         return getattr(self, item)
 
     def keys(self):
-        return ['client_id', 'code', 'redirect_uri', 'scopes']
+        return ['client_id', 'code', 'redirect_uri', 'scopes', 'user']
 
 
 class GrantCacheBinding(object):
-    """
+    """Configures an :class:`OAuth2Provider` instance to use various caching
+    systems to get and set the grant token. This removes the need to
+    register :func:`grantgetter` and :func:`grantsetter` yourself.
+
+    :param app: Flask application instance
+    :param provider: :class:`OAuth2Provider` instance
+    :param current_user: function that returns an :class:`User` object
+    :param config: Additional configuration
+
+    A usage example::
+
+        oauth = OAuth2Provider(app)
+        config = {'OAUTH2_CACHE_TYPE': 'redis'}
+
+        GrantCacheBinding(app, oauth, current_user, config=config)
+
+    You can define which cache system you would like to use by setting the
+    following configuration option::
+
+        OAUTH2_CACHE_TYPE = 'null' // memcache, simple, redis, filesystem
+
+    For more information on the supported cache systems please visit:
+    `Cache <http://werkzeug.pocoo.org/docs/contrib/cache/>`_
+
     """
 
     def __init__(self, app, provider, current_user, config=None):
 
         if config is None:
             config = app.config
+        else:
+            from itertools import chain
+            config = dict(chain(app.config.items(), config.items()))
 
         self.current_user = current_user
 
-        config.setdefault('OAUTH2_CACHE_DEFAULT_TIMEOUT', 100)
-        config.setdefault('OAUTH2_CACHE_THRESHOLD', 500)
-        config.setdefault('OAUTH2_CACHE_KEY_PREFIX', None)
-        config.setdefault('OAUTH2_CACHE_MEMCACHED_SERVERS', None)
-        config.setdefault('OAUTH2_CACHE_REDIS_HOST', 'localhost')
-        config.setdefault('OAUTH2_CACHE_REDIS_PORT', 6379)
-        config.setdefault('OAUTH2_CACHE_REDIS_PASSWORD', None)
-        config.setdefault('OAUTH2_CACHE_REDIS_DB', 0)
-        config.setdefault('OAUTH2_CACHE_DIR', None)
-        config.setdefault('OAUTH2_CACHE_MODE', '0600')
-        config.setdefault('OAUTH2_CACHE_TYPE', 'null')
+        settings = (
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_DEFAULT_TIMEOUT',
+                'flask_cache_key': 'CACHE_DEFAULT_TIMEOUT',
+                'default_value': 100
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_THRESHOLD',
+                'flask_cache_key': 'CACHE_THRESHOLD',
+                'default_value': 500
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_KEY_PREFIX',
+                'flask_cache_key': 'CACHE_KEY_PREFIX',
+                'default_value': None
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_MEMCACHED_SERVERS',
+                'flask_cache_key': 'CACHE_MEMCACHED_SERVERS',
+                'default_value': None
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_REDIS_HOST',
+                'flask_cache_key': 'CACHE_REDIS_HOST',
+                'default_value': 'localhost'
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_REDIS_PORT',
+                'flask_cache_key': 'CACHE_REDIS_PORT',
+                'default_value': 6379
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_REDIS_PASSWORD',
+                'flask_cache_key': 'CACHE_REDIS_PASSWORD',
+                'default_value': None
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_REDIS_DB',
+                'flask_cache_key': 'CACHE_REDIS_DB',
+                'default_value': 0
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_DIR',
+                'flask_cache_key': 'CACHE_DIR',
+                'default_value': None
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_MODE',
+                'flask_cache_key': 'CACHE_MODE',
+                'default_value': '0600'
+            },
+            {
+                'flask_oauthlib_key': 'OAUTH2_CACHE_TYPE',
+                'flask_cache_key': 'CACHE_TYPE',
+                'default_value': 'null'
+            },
+        )
+
+        for setting in settings:
+            flask_oauthlib_key = setting['flask_oauthlib_key']
+            flask_cache_key = setting['flask_cache_key']
+            if flask_cache_key in config and flask_oauthlib_key not in config:
+                config[flask_oauthlib_key] = config[flask_cache_key]
+            else:
+                config.setdefault(flask_oauthlib_key, setting['default_value'])
 
         self.config = config
         kwargs = dict(default_timeout=config['OAUTH2_CACHE_DEFAULT_TIMEOUT'])
@@ -106,13 +190,22 @@ class GrantCacheBinding(object):
         provider.grantsetter(self.set)
 
     def _null(self, kwargs):
+        """Returns a :class:`NullCache` instance"""
         return NullCache()
 
     def _simple(self, kwargs):
+        """Returns a :class:`SimpleCache` instance
+
+        .. warning::
+
+            This cache system might not be thread safe. Use with caution.
+
+        """
         kwargs.update(dict(threshold=self.config['OAUTH2_CACHE_THRESHOLD']))
         return SimpleCache(**kwargs)
 
     def _memcache(self, kwargs):
+        """Returns a :class:`MemcachedCache` instance"""
         kwargs.update(dict(
             servers=self.config['OAUTH2_CACHE_MEMCACHED_SERVERS'],
             key_prefix=self.config['OAUTH2_CACHE_KEY_PREFIX']
@@ -120,6 +213,7 @@ class GrantCacheBinding(object):
         return MemcachedCache(**kwargs)
 
     def _redis(self, kwargs):
+        """Returns a :class:`RedisCache` instance"""
         kwargs.update(dict(
             host=self.config['OAUTH2_CACHE_REDIS_HOST'],
             port=self.config['OAUTH2_CACHE_REDIS_PORT'],
@@ -130,13 +224,14 @@ class GrantCacheBinding(object):
         return RedisCache(**kwargs)
 
     def _filesystem(self, kwargs):
+        """Returns a :class:`FileSystemCache` instance"""
         kwargs.update(dict(
             threshold=self.config['OAUTH2_CACHE_THRESHOLD']
         ))
         return FileSystemCache(self.config['OAUTH2_CACHE_DIR'], **kwargs)
 
     def set(self, client_id, code, request, *args, **kwargs):
-        log.debug("SET GRANT")
+        """Sets the grant token with the configured cache system"""
         grant = Grant(
             self.cache,
             client_id=request.client.client_id,
@@ -145,23 +240,71 @@ class GrantCacheBinding(object):
             scopes=request.scopes,
             user=self.current_user()
         )
+        log.debug("Set Grant Token with key {0}".format(grant.key))
         self.cache.set(grant.key, dict(grant))
 
     def get(self, client_id, code):
-        log.debug("GET GRANT")
-        grant = Grant(self.cache, client_id=client_id,
-                      code=code, user=self.current_user())
+        """Gets the grant token with the configured cache system"""
+        grant = Grant(self.cache, client_id=client_id, code=code)
         kwargs = self.cache.get(grant.key)
-        log.debug("KWARGS: {0}".format(kwargs))
         if kwargs:
+            log.debug("Grant Token found with key {0}".format(grant.key))
             for k, v in kwargs.iteritems():
                 setattr(grant, k, v)
             return grant
-        log.debug("GET NOT FOUND")
+        log.debug("Grant Token not found with key {0}".format(grant.key))
         return None
 
 
 class SQLAlchemyBinding(object):
+    """Configures the given :class:`OAuth2Provider` instance with the
+    required getters and setters for persistence with SQLAlchemy.
+
+    An example of using all models::
+
+        oauth = OAuth2Provider(app)
+
+        SQLAlchemyBinding(oauth, get_session, user=User, client=Client,
+                          token=Token, grant=Grant, current_user=current_user)
+
+    You can omit any model if you wish to register the functions yourself.
+    It is also possible to override the functions by registering them
+    afterwards::
+
+        oauth = OAuth2Provider(app)
+
+        SQLAlchemyBinding(oauth, get_session, user=User, client=Client,
+                          token=Token)
+
+        @oauth.grantgetter
+        def get_grant(client_id, code):
+            pass
+
+        @oauth.grantsetter
+        def set_grant(client_id, code, request, *args, **kwargs):
+            pass
+
+        # register tokensetter with oauth but keeping the tokengetter
+        # registered by `SQLAlchemyBinding`
+        # You would only do this for the token and grant since user and client
+        # only have getters
+        @oauth.tokensetter
+        def set_token(token, request, *args, **kwargs):
+            pass
+
+    Note that current_user is only required if you're using SQLAlchemy
+    for grant caching. If you're using another caching system with
+    GrantCacheBinding instead, omit current_user.
+
+    :param provider: :class:`OAuth2Provider` instance
+    :param get_session: a function that returns a :class:`Session` object
+    :param user: :class:`User` model
+    :param client: :class:`Client` model
+    :param token: :class:`Token` model
+    :param grant: :class:`Grant` model
+    :param current_user: function that returns a :class:`User` object
+
+    """
 
     def __init__(self, provider, get_session, user=None, client=None,
                  token=None, grant=None, current_user=None):
@@ -189,15 +332,19 @@ class SQLAlchemyBinding(object):
 
 
 class BaseBinding(object):
+    """Base Binding
+
+    :param model: SQLAlchemy Model class
+    :param get_session: a functiont that returns a :class:`Session` object
+    """
 
     def __init__(self, model, get_session):
         self.get_session = get_session
-        # self.query = _QueryObject(model, get_session)
         self.model = model
 
     @property
     def query(self):
-        log.debug('GET QUERY')
+        """Determines which method of getting the query object for use"""
         if hasattr(self.model, 'query'):
             return self.model.query
         else:
@@ -205,8 +352,16 @@ class BaseBinding(object):
 
 
 class UserBinding(BaseBinding):
+    """Object use by SQLAlchemyBinding to register the user getter"""
 
     def get(self, username, password, *args, **kwargs):
+        """Returns the User object
+
+        Returns None if the user isn't found or the passwords don't match
+
+        :param username: username of the user
+        :param password: password of the user
+        """
         user = self.query.filter_by(username=username).first()
         if user.check_password(password):
             return user
@@ -214,15 +369,27 @@ class UserBinding(BaseBinding):
 
 
 class ClientBinding(BaseBinding):
+    """Object use by SQLAlchemyBinding to register the client getter"""
 
     def get(self, client_id):
-        # log.debug("QUERY {}".format(self.query))
+        """Returns a Client object with the given client ID
+
+        :param client_id: ID if the client
+        """
         return self.query.filter_by(client_id=client_id).first()
 
 
 class TokenBinding(BaseBinding):
+    """Object use by SQLAlchemyBinding to register the token
+    getter and setter
+    """
 
     def get(self, access_token=None, refresh_token=None):
+        """returns a Token object with the given access token or refresh token
+
+        :param access_token: User's access token
+        :param refresh_token: User's refresh token
+        """
         if access_token:
             return self.query.filter_by(access_token=access_token).first()
         elif refresh_token:
@@ -230,6 +397,12 @@ class TokenBinding(BaseBinding):
         return None
 
     def set(self, token, request, *args, **kwargs):
+        """Creates a Token object and removes all expired tokens that belong
+        to the user
+
+        :param token: token object
+        :param request: OAuthlib request object
+        """
         session = self.get_session()
         tokens = self.query.filter_by(client_id=request.client.client_id,
                                       user_id=request.user.id).all()
@@ -252,12 +425,21 @@ class TokenBinding(BaseBinding):
 
 
 class GrantBinding(BaseBinding):
+    """Object use by SQLAlchemyBinding to register the grant
+    getter and setter
+    """
 
     def __init__(self, model, session, current_user):
         self.current_user = current_user
         super(GrantBinding, self).__init__(model, session)
 
     def set(self, client_id, code, request, *args, **kwargs):
+        """Creates Grant object with the given params
+
+        :param client_id: ID of the client
+        :param code:
+        :param request: OAuthlib request object
+        """
         session = self.get_session()
         expires = datetime.utcnow() + timedelta(seconds=100)
         grant = self.model(
@@ -273,4 +455,9 @@ class GrantBinding(BaseBinding):
         session.commit()
 
     def get(self, client_id, code):
+        """Get the Grant object with the given client ID and code
+
+        :param client_id: ID of the client
+        :param code:
+        """
         return self.query.filter_by(client_id=client_id, code=code).first()
