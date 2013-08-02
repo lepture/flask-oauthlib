962d881bc98e2701b80802a1fc7bcfb385b835ca
Hsiaoming Yang <me@lepture.com>
2013-08-02 08:02:48 +0800

    add example for linkedin

---
+++ example/linkedin.py
@@ -0,0 +1,64 @@
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+linkedin = oauth.remote_app(
+    'linkedin',
+    consumer_key='k8fhkgkkqzub',
+    consumer_secret='ZZtLETQOQYNDjMrz',
+    request_token_params={
+        'scope': 'r_basicprofile',
+        'state': 'RandomString',
+    },
+    base_url='https://api.linkedin.com/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://www.linkedin.com/uas/oauth2/accessToken',
+    authorize_url='https://www.linkedin.com/uas/oauth2/authorization',
+)
+
+
+@app.route('/')
+def index():
+    if 'linkedin_token' in session:
+        me = linkedin.get('people/~')
+        return jsonify(me.data)
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return linkedin.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('linkedin_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+@linkedin.authorized_handler
+def authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['linkedin_token'] = (resp['access_token'], '')
+    me = linkedin.get('people/~')
+    return jsonify(me.data)
+
+
+@linkedin.tokengetter
+def get_linkedin_oauth_token():
+    return session.get('linkedin_token')
+
+
+if __name__ == '__main__':
+    app.run()
