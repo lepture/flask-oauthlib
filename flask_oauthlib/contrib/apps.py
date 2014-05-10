6bb4810eddedf5aa26f8c3e20115591b3f4a434d
Jiangge Zhang <tonyseek@gmail.com>
2014-05-11 01:57:05 +0800

    refine the docstring processing and add facebook, dropbox, github.

--- flask_oauthlib/contrib/apps.py
+++ flask_oauthlib/contrib/apps.py
@@ -12,7 +12,7 @@ class RemoteAppFactory(object):
         self.default_name = default_name
         self.kwargs = kwargs
         self._kwargs_processor = None
-        self.__doc__ = docstring
+        self.__doc__ = docstring.lstrip()
 
     def register_to(self, oauth, name=None, **kwargs):
         """Creates a remote app and registers it."""
@@ -29,9 +29,6 @@ class RemoteAppFactory(object):
     def kwargs_processor(self, fn):
         """Sets a function to process kwargs before creating any app."""
         self._kwargs_processor = fn
-        if fn.__doc__:
-            # appends docstring
-            self.__doc__ = '%s\n%s' % (self.__doc__, fn.__doc__)
         return fn
 
     def _process_kwargs(self, **kwargs):
@@ -46,26 +43,66 @@ class RemoteAppFactory(object):
         return final_kwargs
 
 
+def make_scope_processor(default_scope):
+    def processor(**kwargs):
+        # request_token_params
+        scope = kwargs.pop('scope', [default_scope])  # default scope
+        if not isinstance(scope, basestring):
+            scope = ','.join(scope)  # allows list-style scope
+        request_token_params = kwargs.setdefault('request_token_params', {})
+        request_token_params.setdefault('scope', scope)  # doesn't override
+        return kwargs
+    return processor
+
+
 douban = RemoteAppFactory('douban', {
     'base_url': 'https://api.douban.com/v2/',
+    'request_token_url': None,
     'access_token_url': 'https://www.douban.com/service/auth2/token',
     'authorize_url': 'https://www.douban.com/service/auth2/auth',
     'access_token_method': 'POST',
-}, """The OAuth app for douban.com API.""")
+}, """
+The OAuth app for douban.com API.
+
+:param scope: optional. default: ['douban_basic_common'].
+              see also: http://developers.douban.com/wiki/?title=oauth2
+""")
+douban.kwargs_processor(make_scope_processor('douban_basic_common'))
 
 
-@douban.kwargs_processor
-def douban_kwargs_processor(**kwargs):
-    """
-    :param scope: optional. default: ['douban_basic_common'].
-                  see also: http://developers.douban.com/wiki/?title=oauth2
-    """
-    # request_token_url
-    kwargs.setdefault('request_token_url', None)
-    # request_token_params
-    scope = kwargs.pop('scope', ['douban_basic_common'])  # default scope
-    if not isinstance(scope, basestring):
-        scope = ','.join(scope)  # allows list-style scope
-    request_token_params = kwargs.setdefault('request_token_params', {})
-    request_token_params.setdefault('scope', scope)  # doesn't override exists
-    return kwargs
+dropbox = RemoteAppFactory('dropbox', {
+    'base_url': 'https://www.dropbox.com/1/',
+    'request_token_url': None,
+    'access_token_url': 'https://api.dropbox.com/1/oauth2/token',
+    'authorize_url': 'https://www.dropbox.com/1/oauth2/authorize',
+    'access_token_method': 'POST',
+    'request_token_params': {},
+}, """The OAuth app for Dropbox API.""")
+
+
+facebook = RemoteAppFactory('facebook', {
+    'request_token_params': {'scope': 'email'},
+    'base_url': 'https://graph.facebook.com',
+    'request_token_url': None,
+    'access_token_url': '/oauth/access_token',
+    'authorize_url': 'https://www.facebook.com/dialog/oauth',
+}, """
+The OAuth app for Facebook API.
+
+:param scope: optional. default: ['email'].
+""")
+facebook.kwargs_processor(make_scope_processor('email'))
+
+
+github = RemoteAppFactory('github', {
+    'base_url': 'https://api.github.com/',
+    'request_token_url': None,
+    'access_token_method': 'POST',
+    'access_token_url': 'https://github.com/login/oauth/access_token',
+    'authorize_url': 'https://github.com/login/oauth/authorize',
+}, """
+The OAuth app for GitHub API.
+
+:param scope: optional. default: ['user:email'].
+""")
+github.kwargs_processor(make_scope_processor('user:email'))
