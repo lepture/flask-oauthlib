808e4c8b52705d87856523393e9672b56fc8c172
Christofer Bertonha <christofer.bertonha@titansgroup.com.br>
2014-07-22 18:37:14 -0300

    Fix install_requires in setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     license='BSD',
     install_requires=[
         'Flask',
-        'oauthlib=>0.6.2',
+        'oauthlib>=0.6.2',
     ],
     tests_require=['nose', 'Flask-SQLAlchemy', 'mock'],
     test_suite='nose.collector',
