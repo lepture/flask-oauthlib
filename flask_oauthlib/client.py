cfad30a99627fb1518dd61f96e61ae12f73163be
mdxs <van.wijgerden@mdxs.net>
2014-09-15 09:59:41 +0200

    Textual improvement
    Using `in favor of` instead of `in favor for`; see http://en.wiktionary.org/wiki/in_favor_of

--- flask_oauthlib/client.py
+++ flask_oauthlib/client.py
@@ -678,12 +678,12 @@ class OAuthRemoteApp(object):
         """Handles an OAuth callback.
 
         .. versionchanged:: 0.7
-           @authorized_handler is deprecated in favor for authorized_response.
+           @authorized_handler is deprecated in favor of authorized_response.
         """
         @wraps(f)
         def decorated(*args, **kwargs):
             log.warn(
-                '@authorized_handler is deprecated in favor for '
+                '@authorized_handler is deprecated in favor of '
                 'authorized_response'
             )
             data = self.authorized_response()
