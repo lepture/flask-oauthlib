ece4d8fe75bb17cf62e3df68147e0305c092c10a
Jiangge Zhang <tonyseek@gmail.com>
2014-12-28 15:48:08 +0800

    add support for automatic token refresh.

--- example/contrib/experiment-client/douban.py
+++ example/contrib/experiment-client/douban.py
@@ -23,13 +23,14 @@ douban = oauth.remote_app(
     version='2',
     endpoint_url='https://api.douban.com/',
     access_token_url='https://www.douban.com/service/auth2/token',
+    refresh_token_url='https://www.douban.com/service/auth2/token',
     authorization_url='https://www.douban.com/service/auth2/auth',
     compliance_fixes='.douban:douban_compliance_fix')
 
 
 @app.route('/')
 def home():
-    if oauth_douban_token():
+    if obtain_douban_token():
         response = douban.get('v2/user/~me')
         return jsonify(response=response.json())
     return '<a href="%s">Login</a>' % url_for('oauth_douban')
@@ -45,15 +46,20 @@ def oauth_douban():
 def oauth_douban_callback():
     response = douban.authorized_response()
     if response:
-        session['token'] = response
+        store_douban_token(response)
         return repr(dict(response))
     else:
         return '<a href="%s">T_T Denied</a>' % (url_for('oauth_douban'))
 
 
 @douban.tokengetter
-def oauth_douban_token():
+def obtain_douban_token():
     return session.get('token')
+
+
+@douban.tokensaver
+def store_douban_token(token):
+    session['token'] = token
 
 
 if __name__ == '__main__':
