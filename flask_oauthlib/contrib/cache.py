443d2de9cc2345022c5fac5d1a4ea02788ce7d2e
Hsiaoming Yang <me@lepture.com>
2015-02-03 19:00:43 +0800

    Register cache to app.extensions

--- flask_oauthlib/contrib/cache.py
+++ flask_oauthlib/contrib/cache.py
@@ -20,6 +20,7 @@ class Cache(object):
             raise RuntimeError(
                 '`%s` is not a valid cache type!' % cache_type
             )
+        app.extensions[config_prefix.lower() + '_cache'] = self.cache
 
     def __getattr__(self, key):
         try:
