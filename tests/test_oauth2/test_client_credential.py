3f948694bdb8371a97c049638e3b930ec7764678
Hsiaoming Yang <me@lepture.com>
2015-04-02 15:16:51 +0800

    Add test client_credential

---
+++ tests/test_oauth2/test_client_credential.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+from .base import TestCase
+from .base import create_server, sqlalchemy_provider, cache_provider
+from .base import db, Client, User
+
+
+class TestDefaultProvider(TestCase):
+    def create_server(self):
+        create_server(self.app)
+
+    def prepare_data(self):
+        self.create_server()
+
+        oauth_client = Client(
+            name='ios', client_id='client', client_secret='secret',
+            _redirect_uris='http://localhost/authorized',
+        )
+
+        db.session.add(User(username='foo'))
+        db.session.add(oauth_client)
+
+        self.oauth_client = oauth_client
+
+    def test_get_token(self):
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'client_credentials',
+            'client_id': self.oauth_client.client_id,
+            'client_secret': self.oauth_client.client_secret,
+        })
+        assert b'access_token' in rv.data
+
+
+class TestSQLAlchemyProvider(TestDefaultProvider):
+    def create_server(self):
+        create_server(self.app, sqlalchemy_provider(self.app))
+
+
+class TestCacheProvider(TestDefaultProvider):
+    def create_server(self):
+        create_server(self.app, cache_provider(self.app))
