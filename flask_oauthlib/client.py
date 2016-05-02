75c870bd1bbfd239c04aedfe4d8f3e45f31a490c
Ricky Cook <mail@thatpanda.com>
2016-05-02 13:19:08 +1000

    Reimport session into client

--- flask_oauthlib/client.py
+++ flask_oauthlib/client.py
@@ -14,7 +14,7 @@ import oauthlib.oauth2
 from copy import copy
 from functools import wraps
 from oauthlib.common import to_unicode, PY3, add_params_to_uri
-from flask import request, redirect, json, current_app
+from flask import request, redirect, json, session, current_app
 from werkzeug import url_quote, url_decode, url_encode
 from werkzeug import parse_options_header, cached_property
 from .utils import to_bytes
