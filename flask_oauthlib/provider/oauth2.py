b5dce0d87f2dc15b2f06cd16c48e6c4fcafa46a1
Ranjan Kumar Patel <ranjan.patel@thinkcareersindia.com>
2020-02-08 14:55:16 +0530

    oauth lib

--- flask_oauthlib/provider/oauth2.py
+++ flask_oauthlib/provider/oauth2.py
@@ -8,18 +8,19 @@
     :copyright: (c) 2013 - 2014 by Hsiaoming Yang.
 """
 
-import os
-import logging
 import datetime
+import logging
+import os
 from functools import wraps
-from flask import request, url_for
+
 from flask import redirect, abort
-from werkzeug import cached_property
-from werkzeug.utils import import_string
+from flask import request, url_for
 from oauthlib import oauth2
+from oauthlib.common import add_params_to_uri
 from oauthlib.oauth2 import RequestValidator, Server
-from oauthlib.common import to_unicode, add_params_to_uri
-from ..utils import extract_params, decode_base64, create_response
+from werkzeug.utils import import_string, cached_property
+
+from ..utils import extract_params, create_response
 
 __all__ = ('OAuth2Provider', 'OAuth2RequestValidator')
 
@@ -155,10 +156,10 @@ class OAuth2Provider(object):
             )
 
         if hasattr(self, '_clientgetter') and \
-           hasattr(self, '_tokengetter') and \
-           hasattr(self, '_tokensetter') and \
-           hasattr(self, '_grantgetter') and \
-           hasattr(self, '_grantsetter'):
+                hasattr(self, '_tokengetter') and \
+                hasattr(self, '_tokensetter') and \
+                hasattr(self, '_grantgetter') and \
+                hasattr(self, '_grantsetter'):
 
             usergetter = None
             if hasattr(self, '_usergetter'):
@@ -413,6 +414,7 @@ class OAuth2Provider(object):
                 confirm = request.form.get('confirm', 'no')
                 return confirm == 'yes'
         """
+
         @wraps(f)
         def decorated(*args, **kwargs):
             # raise if server not implemented
@@ -472,8 +474,9 @@ class OAuth2Provider(object):
                 # denied by user
                 e = oauth2.AccessDeniedError(state=request.values.get('state'))
                 return self._on_exception(e, e.in_uri(redirect_uri))
-              
+
             return self.confirm_authorization_request()
+
         return decorated
 
     def confirm_authorization_request(self):
@@ -502,7 +505,7 @@ class OAuth2Provider(object):
             return self._on_exception(e, e.in_uri(self.error_uri))
         except oauth2.OAuth2Error as e:
             log.debug('OAuth2Error: %r', e, exc_info=True)
-            
+
             # on auth error, we should preserve state if it's present according to RFC 6749
             state = request.values.get('state')
             if state and not e.state:
@@ -545,6 +548,7 @@ class OAuth2Provider(object):
             def access_token():
                 return None
         """
+
         @wraps(f)
         def decorated(*args, **kwargs):
             server = self.server
@@ -555,6 +559,7 @@ class OAuth2Provider(object):
                 uri, http_method, body, headers, credentials
             )
             return create_response(*ret)
+
         return decorated
 
     def revoke_handler(self, f):
@@ -574,6 +579,7 @@ class OAuth2Provider(object):
 
         .. _`RFC7009`: http://tools.ietf.org/html/rfc7009
         """
+
         @wraps(f)
         def decorated(*args, **kwargs):
             server = self.server
@@ -587,10 +593,12 @@ class OAuth2Provider(object):
             ret = server.create_revocation_response(
                 uri, headers=headers, body=body, http_method=http_method)
             return create_response(*ret)
+
         return decorated
 
     def require_oauth(self, *scopes):
         """Protect resource with specified scopes."""
+
         def wrapper(f):
             @wraps(f)
             def decorated(*args, **kwargs):
@@ -611,7 +619,9 @@ class OAuth2Provider(object):
                     return abort(401)
                 request.oauth = req
                 return f(*args, **kwargs)
+
             return decorated
+
         return wrapper
 
 
@@ -624,6 +634,7 @@ class OAuth2RequestValidator(RequestValidator):
     :param grantgetter: a function to get grant token
     :param grantsetter: a function to save grant token
     """
+
     def __init__(self, clientgetter, tokengetter, grantgetter,
                  usergetter=None, tokensetter=None, grantsetter=None):
         self._clientgetter = clientgetter
@@ -674,6 +685,7 @@ class OAuth2RequestValidator(RequestValidator):
         .. _`Section 4.1.3`: http://tools.ietf.org/html/rfc6749#section-4.1.3
         .. _`Section 6`: http://tools.ietf.org/html/rfc6749#section-6
         """
+
         def is_confidential(client):
             if hasattr(client, 'is_confidential'):
                 return client.is_confidential
@@ -901,7 +913,7 @@ class OAuth2RequestValidator(RequestValidator):
             log.debug('Grant not found.')
             return False
         if hasattr(grant, 'expires') and \
-           datetime.datetime.utcnow() > grant.expires:
+                datetime.datetime.utcnow() > grant.expires:
             log.debug('Grant is expired.')
             return False
 
